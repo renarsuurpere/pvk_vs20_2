2531fc660ed3f94d5fe1c9f7ebefd763cd02305c
Renar Suurpere <renar.suurpere@khk.ee>
2021-02-12 12:42:10 +0200

    Lisatud kasutsusele rakendusega seotud konstandid

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,4 +1,6 @@
 <?php
+// config file
+require_once 'config/constants.php';
 // load libraries
 require_once 'libs/Core.php';
 require_once 'libs/Controller.php';
---
+++ app/config/constants.php
@@ -0,0 +1,7 @@
+<?php
+// Project name
+define ('PROJECT_NAME', 'pvk_vs20_2');
+// app root dir
+define ('APPROOT', dirname(dirname(__FILE__)));
+// URL root
+define('URLROOT', 'http://'.$_SERVER['HTTP_HOST'].'/'.PROJECT_NAME);
